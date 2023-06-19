27a6e6dbbb7784ba105a67b84dd54a76ec9c0772
AmirYakubov <amir@electricgames.io>
2023-06-19 20:26:08 +0300

    fix API

--- src/adapters/GemelnetAdapter.ts
+++ src/adapters/GemelnetAdapter.ts
@@ -10,7 +10,9 @@ export class GemelnetAdapter {
   public async getData(){
     const BASE_URL = 'https://data.gov.il/api/3/action/datastore_search';
     const { data } = await lastValueFrom(this.httpService.get(
-      BASE_URL + '?resource_id=a30dcbea-a1d2-482c-ae29-8f781f5025fb' + '&limit=5'));
+      BASE_URL + '?resource_id=a30dcbea-a1d2-482c-ae29-8f781f5025fb' + '&q=קרנות השתלמות' + '&q=מגדל השתלמות מסלול שקלי טווח קצר'));
     return data.result.records;
   }
 }
+
+//'&limit=5'+'&q=הראל'
