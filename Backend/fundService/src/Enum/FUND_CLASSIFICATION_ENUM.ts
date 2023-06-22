b677fadd957c744f608e9492c3ed2e82a1534a5d
AmirYakubov <amir@electricgames.io>
2023-06-22 12:39:23 +0300

    sync branches

--- Backend/fundService/src/Enum/FUND_CLASSIFICATION_ENUM.ts
+++ Backend/fundService/src/Enum/FUND_CLASSIFICATION_ENUM.ts
@@ -1,5 +1,5 @@
 export enum FUND_CLASSIFICATION_ENUM {
     EDUCATION_FUND = 'קרנות השתלמות',
     PROVIDENT_FUND = 'גמל להשקעה',
-    CHILDREN_FUND = 'חסכון לכל ילד'
+    CHILDREN_FUND = 'חסכון לילד'
 }
